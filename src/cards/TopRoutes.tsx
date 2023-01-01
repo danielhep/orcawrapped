186ac3dfb554fb05ef6d6fb852f03becea8c2650
Daniel Heppner <dh@danielheppner.com>
2022-12-31 16:58:49 -0800

    improve bar chart design

--- src/cards/TopRoutes.tsx
+++ src/cards/TopRoutes.tsx
@@ -3,11 +3,57 @@ import { Group } from "@visx/group";
 import { ParentSize } from "@visx/responsive";
 import { scaleBand, scaleLinear } from "@visx/scale";
 import { Bar } from "@visx/shape";
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { AppState, WrappedCard } from "../types";
 
+function BarElement({ line, yScale, xScale, maxWidth }) {
+  const TEXT_PADDING = 10;
+  const OVERHANG = 40;
+  const textRef = useRef<SVGTextElement>(null);
+  const [textWidth, setTextWidth] = useState(0);
+  const barWidth = xScale(line.count) + OVERHANG;
+  const barHeight = yScale.bandwidth() - 10;
+  const barY = (yScale(line.id) ?? 0) - barHeight / 2;
+  const barX = -OVERHANG;
+  const textX = barX + barWidth + TEXT_PADDING;
+  const adjustedTextX =
+    textX + textWidth + TEXT_PADDING > maxWidth
+      ? textX - (textWidth + TEXT_PADDING + textX - maxWidth)
+      : textX;
+  useLayoutEffect(() => {
+    setTextWidth(textRef.current?.getBBox().width ?? 0);
+  }, []);
+  return (
+    <React.Fragment key={`${line.line ?? "unknown"}-${line.agencyName}`}>
+      <Bar
+        key={`bar-${line.line ?? "unknown"}-${line.agencyName}`}
+        width={barWidth}
+        height={barHeight}
+        y={barY}
+        x={barX}
+        rx={20}
+        fill="url(#barGradient)"
+        opacity={0.5}
+      />
+      <text
+        ref={textRef}
+        dominantBaseline="middle"
+        y={yScale(`${line.line ?? "unknown"}-${line.agencyName}`)}
+        x={adjustedTextX}
+        style={{ fill: "white", fontSize: "2em" }}
+      >
+        {line.routeShortName ?? "<Unknown>"}
+      </text>
+    </React.Fragment>
+  );
+}
+
 function FavoriteRoutes({ state }: { state: AppState }): JSX.Element {
-  const data = state?.extraData?.routeOccurrences.slice(0, 5) ?? [];
+  const data = state?.extraData?.routeOccurrences.slice(0, 4) ?? [];
+  const dataWithIDs = data.map((d) => ({
+    ...d,
+    id: `${d.line ?? "unknown"}-${d.agencyName}`,
+  }));
 
   return (
     <div
@@ -19,17 +65,17 @@ function FavoriteRoutes({ state }: { state: AppState }): JSX.Element {
     >
       <ParentSize>
         {(parent) => {
-          const margin = 0.1 * parent.height;
           const yScale = scaleBand({
-            domain: data.map((d) => `${d.line ?? "unknown"}-${d.agencyName}`),
-            range: [margin, parent.height - margin],
+            domain: dataWithIDs.map((d) => d.id),
+            range: [80, parent.height - 20],
             padding: 0,
             round: true,
           });
 
           const xScale = scaleLinear({
-            range: [0, parent.width],
+            range: [0, parent.width - 20],
             round: true,
+            nice: true,
             domain: [0, Math.max(...data.map((d) => d.count))],
           });
 
@@ -54,48 +100,34 @@ function FavoriteRoutes({ state }: { state: AppState }): JSX.Element {
                 fill="url('#gradient')"
               />
               <text
-                textAnchor="middle"
-                dominantBaseline="middle"
-                y={30}
-                x={parent.width / 2}
-                style={{ fill: "white", fontWeight: "bold", fontSize: "20pt" }}
+                y={40}
+                x={10}
+                style={{ fill: "white", fontWeight: "bold", fontSize: "30px" }}
               >
-                2022 TOP 5 LINES
+                TOP ROUTES
+              </text>
+              <text
+                textAnchor="end"
+                y={40 + 30 + 10}
+                x={parent.width - 10}
+                style={{
+                  fill: "white",
+                  fontWeight: "bold",
+                  fontSize: "30px",
+                }}
+              >
+                OF 2022
               </text>
               <Group top={60}>
-                {data.map((line, i) => {
-                  const barWidth = xScale(line.count);
-                  const barHeight = yScale.bandwidth();
-                  const barY =
-                    (yScale(`${line.line ?? "unknown"}-${line.agencyName}`) ??
-                      0) -
-                    barHeight / 2;
-                  return (
-                    <React.Fragment
-                      key={`${line.line ?? "unknown"}-${line.agencyName}`}
-                    >
-                      <Bar
-                        key={`bar-${line.line ?? "unknown"}-${line.agencyName}`}
-                        width={barWidth}
-                        height={barHeight}
-                        y={barY}
-                        x={0}
-                        fill="url(#barGradient)"
-                        opacity={0.5}
-                      />
-                      <text
-                        dominantBaseline="middle"
-                        y={yScale(
-                          `${line.line ?? "unknown"}-${line.agencyName}`
-                        )}
-                        x={10}
-                        style={{ fill: "white" }}
-                      >
-                        {line.routeShortName ?? "<Unknown>"}
-                      </text>
-                    </React.Fragment>
-                  );
-                })}
+                {dataWithIDs.map((line, i) => (
+                  <BarElement
+                    line={line}
+                    yScale={yScale}
+                    xScale={xScale}
+                    key={line.id}
+                    maxWidth={parent.width}
+                  />
+                ))}
               </Group>
             </svg>
           );
