343e7ec15c5bac8c7f85b62b317244791f18bf47
Daniel Heppner <dh@danielheppner.com>
2023-05-24 17:37:54 -0700

    update scores to fix order

--- src/cards/Calendar/index.ts
+++ src/cards/Calendar/index.ts
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 import { WrappedCard } from "../../types";
 
 export default {
-  score: () => 1000,
+  score: () => 998,
   cardName: "Calendar",
   Component: dynamic(() => import("./Calendar"), { ssr: false }),
 } as WrappedCard;
